dffe06a5e2b4e61a67dc44113c92bb413181cd40
wudi7mi <wudi7mi@gmail.com>
2024-01-14 00:27:05 +0800

    嵌入字体

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QFontDatabase>
 #include <QLocale>
 #include <QMessageBox>
 #include <QObject>
@@ -13,6 +14,9 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QFontDatabase::addApplicationFont(":/fonts/assets/fonts/OPlusSans3-Medium.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/assets/fonts/OPlusSans3-Regular.ttf");
 
     NEFilter filter;
     a.installNativeEventFilter(&filter);
